c73a2667c6959ac53410df5ed49ef1b6a7538454
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-08-09 17:31:28 +0300

    activerecord validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
-  
+  has_one_attached :attachment
+  validates :attachment, presence: true, blob: { content_type: ['application/pdf'], size_range: 1..5.megabytes }
+  validates :title, presence: true
+  validates :description, presence: true
+ 
+
 end
