911a58555e11b00b8fd2aec1868db3ba93d7598e
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-08-11 10:12:24 +0300

    activerecord validations added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :attachment
-  validates :attachment, presence: true, blob: { content_type: ['application/pdf'], size_range: 1..5.megabytes }
+  validates :attachment, presence: true, blob: { content_type: ['application/pdf'], size_range: 1..15.megabytes }
   validates :title, presence: true
+  validates :title, length: { in: 5..30 } 
   validates :description, presence: true
+  validates :description, length: { in: 5..200 } 
+  validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :books
   validates :name, presence: true
+  validates :name, length: { in: 5..50 } 
+ 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
